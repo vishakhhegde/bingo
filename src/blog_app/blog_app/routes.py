7d24306dada245a230373ec4fce8d7b0991e83dd
Vishakh Hegde <vishakhhegde@Vishakhs-MacBook-Pro.local>
2021-04-19 18:46:42 -0700

    More changes

--- src/blog_app/blog_app/routes.py
+++ src/blog_app/blog_app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, get_flashed_messages, redirect, request
 from blog_app import app, db, bcrypt
-from blog_app.forms import RegistrationForm, LoginForm
+from blog_app.forms import RegistrationForm, LoginForm, UpdateAccountForm 
 from blog_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -57,7 +57,18 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', image_file=image_file, form=form)
